e9cd2d7f9fcca07e95cb4ceb14f43fb220b0f4b4
Emre Yoleri <emreyoleri01@gmail.com>
2022-01-19 16:49:23 +0300

    Video 34 Completed

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -2,10 +2,11 @@ import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import styled from "styled-components";
 import Product from "./Product";
+import { perPage } from "../config";
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -26,8 +27,13 @@ const ProductsListStyles = styled.div`
   grid-gap: 60px;
 `;
 
-const Products = () => {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+const Products = ({ page }) => {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
 
   if (loading) return <p>Loading...</p>;
 
