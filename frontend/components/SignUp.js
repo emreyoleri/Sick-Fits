28388b6536ff7bdc336f60d14927cd879cff81a6
Emre Yoleri <emreyoleri01@gmail.com>
2022-01-20 16:59:39 +0300

    Video 39 Completed

---
+++ frontend/components/SignUp.js
@@ -0,0 +1,90 @@
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import Form from "./styles/Form";
+import useForm from "../lib/useForm";
+import { CURRENT_USER_QUERY } from "./User";
+import Error from "./ErrorMessage";
+
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $name: String!
+    $email: String!
+    $password: String!
+  ) {
+    createUser(data: { name: $name, email: $email, password: $password }) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+const SignUp = () => {
+  const { inputs, handleChange, resetForm } = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+    variables: inputs,
+  });
+
+  console.log(loading);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await signup().catch(console.error);
+    console.log(res);
+    resetForm();
+  };
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Up For an Account</h2>
+      <Error error={error} />
+      <fieldset disabled={loading}>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please Go Head and Sign In
+          </p>
+        )}
+        <label htmlFor="email">
+          Your Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default SignUp;
--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,10 +1,19 @@
 import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+import styled from "styled-components";
+
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
 
 const SignInPage = () => {
   return (
-    <div>
+    <GridStyles>
       <SignIn />
-    </div>
+      <SignUp />
+    </GridStyles>
   );
 };
 
