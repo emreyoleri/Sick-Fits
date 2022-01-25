eef3e2ebb47d6c4cc73d5295a0a472edb4039ab8
Emre Yoleri <emreyoleri01@gmail.com>
2022-01-25 12:08:15 +0300

    Video 50 Completed

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -20,9 +20,13 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+const update = (cache, payload) =>
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+
 const RemoveFromCart = ({ id }) => {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
+    update,
   });
   return (
     <BigButton
