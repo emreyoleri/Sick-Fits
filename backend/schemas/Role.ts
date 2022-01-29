3e345d5639ef5125807b58bc2011ec2ae7b0b540
Emre Yoleri <emreyoleri01@gmail.com>
2022-01-29 13:59:17 +0300

    Video 66 Completed

--- backend/schemas/Role.ts
+++ backend/schemas/Role.ts
@@ -1,7 +1,19 @@
 import { relationship, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
+import { permission } from "../access";
 import { permissionFields } from "./fields";
 export const Role = list({
+  access: {
+    create: permission.canManageRoles,
+    read: permission.canManageRoles,
+    update: permission.canManageRoles,
+    delete: permission.canManageRoles,
+  },
+  ui: {
+    hideCreate: (args) => !permission.canManageRoles(args),
+    hideDelete: (args) => !permission.canManageRoles(args),
+    isHidden: (args) => !permission.canManageRoles(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     ...permissionFields,
