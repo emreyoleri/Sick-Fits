b0c912c9ef445aa5a9656ff5512a12d49067b241
Emre Yoleri <emreyoleri01@gmail.com>
2022-01-29 13:24:38 +0300

    Video 64 Completed

--- backend/access.ts
+++ backend/access.ts
@@ -1,5 +1,22 @@
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
 export const isSignedIn = ({ session }: ListAccessArgs) => {
   return !!session;
+};
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data?.role?.[permission];
+    },
+  ])
+);
+
+export const permission = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs) {
+    return session?.data.name.includes("Emre");
+  },
 };
